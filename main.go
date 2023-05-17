5a3d9f34d9a06339e148278981e49b54d60a1195
Zawar Ahmed <zawarahmed@192.168.10.7>
2023-05-17 15:35:37 +0500

    section 3, lecture 27

--- main.go
+++ main.go
@@ -1,36 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"text/template"
 )
 
 const portNumber = ":8080"
 
-func home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
-}
-func about(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parseTemplate, err := template.ParseFiles("./templates/" + tmpl)
-	if err != nil {
-		fmt.Println("Parsing error", err)
-		return
-	}
-	err = parseTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("Parsing error", err)
-		return
-	}
-}
 func main() {
-	http.HandleFunc("/", home)
-	http.HandleFunc("/about", about)
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 	log.Printf("Starting server on %+s", portNumber)
 	http.ListenAndServe(portNumber, nil)
 }
---
+++ render.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func RenderTemplate(w http.ResponseWriter, tmpl string) {
+	parseTemplate, err := template.ParseFiles("./templates/" + tmpl)
+	if err != nil {
+		fmt.Println("Parsing error", err)
+		return
+	}
+	err = parseTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("Parsing error", err)
+		return
+	}
+}
