8a9be2c2b23cb1913bbe7067deaaca3a16bbbd0f
Zawar Ahmed <zawarahmed@MBOOK-ZAWARAHMED.local>
2023-05-19 11:02:26 +0500

    section 5, lecture 42

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,16 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/Zawar-Ahmed10p/go-web-app/pkg/config"
 	"github.com/Zawar-Ahmed10p/go-web-app/pkg/handlers"
 	"github.com/Zawar-Ahmed10p/go-web-app/pkg/render"
+	"github.com/alexedwards/scs/v2"
 )
 
 const portNumber = ":8080"
 
+var appConf config.Appconfig
+var session *scs.SessionManager
+
 func main() {
-	var appConf config.Appconfig
+
+	appConf.InProduction = false // for prod swithc to true
+	session = scs.New()
+	session.Lifetime = 1 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = appConf.InProduction
+	appConf.Session = session
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("unable to create template cache.")
