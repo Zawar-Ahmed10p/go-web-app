aa5d21a1a1f6adb779a6778f2ae5d036f9151027
Zawar Ahmed <zawarahmed@MBOOK-ZAWARAHMED.local>
2023-05-19 17:18:03 +0500

    section 6, lecture 48

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -36,6 +36,7 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello Again"
 	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+
 	stringMap["remote_ip"] = remoteIP
 	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
 		StringMap: stringMap,
